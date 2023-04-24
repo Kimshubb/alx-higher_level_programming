261d50b0e6bb4be1710f38ecba57aeb0a30e80d9
Kimshubb <issakimanifmk@gmail.com>
2023-04-24 06:18:01 +0000

    print alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 alphabet = ""
 for i in range(97, 123):
-    alphabet += "{}".format(chr(i))
-    print(alphabet, end="")
+    alphabet += (chr(i))
+    print("{}" .format(alphabet), end="")
