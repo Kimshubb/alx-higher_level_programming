31091462ac02eb852d0ec1d79bf7456d0adeeaf4
Kimshubb <issakimanifmk@gmail.com>
2023-04-24 06:27:45 +0000

    print from 0-100

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range (0, 99):
+for i in range (100):
     print("{0:02d}".format(i), end='')
-    print("{:d}".format(99))
+    if i != 99:
+        print(", ", end="")
+print()
