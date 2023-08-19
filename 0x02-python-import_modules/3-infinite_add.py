70bff15119bf0f4792ec750ca256e2fc6f45fdc6
Kimshubb <issakimanifmk@gmail.com>
2023-08-19 09:01:30 +0000

    calculation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
 
     import sys
-    #exclude the first argument which is always the script name
+    """exclude the first argument which is always the script name"""
     args = sys.argv[1:]
     total = sum(map(int, args))
     print(total)
