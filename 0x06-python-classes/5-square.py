2153788f1cca0c3fc2ac843f84d4b28b68766d30
Kimshubb <issakimanifmk@gmail.com>
2023-08-31 08:06:53 +0000

    corrections

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,7 +2,7 @@
 '''Class defination'''
 class Square:
     '''Class square attributes'''
-    def __init__(self, size=0):
+    def __init__(self, size):
         '''Initialize a square
         Args - size int -size of the square
         '''
@@ -18,18 +18,19 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("Size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         ''' Return the current areaof the square'''
-        return(self.__size **2)
+        return(self.__size * self.__size)
     def my_print(self):
         '''Print the square with # character'''
         if self.__size == 0:
             print("")
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for i in range(0, self.__size):
+                [print("#", end="") for j in range(self.__size)]
+		print("")
         
 
