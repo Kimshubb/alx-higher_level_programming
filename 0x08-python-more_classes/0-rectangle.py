740cf656a1011a96d2efd600fc4693b3afef16f5
Kimshubb <issakimanifmk@gmail.com>
2023-09-20 16:17:51 -0700

    corrections

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''Defines a rectangle class'''
+
+
 class Rectangle:
     '''represent a rectangle and its attributes'''
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''Defines a rectangle class'''
+
+
 class Rectangle:
     '''a rectangle class'''
 
