5c45b84a25cc9ccb2e27b26ee086e207de518d71
Kimshubb <issakimanifmk@gmail.com>
2023-10-20 06:40:19 -0700

    tasks

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates table called first_table with values id and name in MYSQL server
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,1 @@
+SELECT column_name, data_type, character_maximum_length FROM information_schema.columns WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
