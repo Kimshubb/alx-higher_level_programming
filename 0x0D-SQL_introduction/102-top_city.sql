07715c9243f4e29e5e4d1bd1a864a990658e8feb
Kimshubb <issakimanifmk@gmail.com>
2023-11-25 06:54:52 -0800

    import and ..

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- displays top of cities temperatures during August and July ordered by temp de-- DESC
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` IN ('7', '8')
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
