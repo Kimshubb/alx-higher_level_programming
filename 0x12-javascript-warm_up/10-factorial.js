ff7735b6114e222b3e32f6d1ba937b37204ad9a6
Kimshubb <issakimanifmk@gmail.com>
2024-01-25 19:34:08 +0000

    fixes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,11 @@ function factorial(n) {
     return 1;
   }
   return n * factorial(n - 1);
-  }
+}
 
 const arg = parseInt(process.argv[2]);
 if (Number.isNaN(arg)) {
   console.log('Factorial of NaN is 1');
 } else {
-  console.log('Factorial:', factorial(arg));
+  console.log(factorial(arg));
 }
