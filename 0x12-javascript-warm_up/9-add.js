c394bce7719491f9b71c9906c831a43a32679d6d
Kimshubb <issakimanifmk@gmail.com>
2024-01-25 19:29:50 +0000

    error fixes

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -12,5 +12,5 @@ const num2 = parseInt(arg2);
 if (!isNaN(num1) && !isNaN(num2)) {
   console.log(add(num1, num2));
 } else {
-  console.log('Invalid input. Please provide two integers.');
+  console.log('NaN');
 }
