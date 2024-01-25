fe17cf46cb8a44bd8582a7ee9916d2c622693ac9
Kimshubb <issakimanifmk@gmail.com>
2024-01-25 19:40:51 +0000

    fixes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice[2];
+const args = process.argv.slice(2);
 const numbers = args.map(Number);
 const sortedNumbers = numbers.sort((a, b) => b - a);
 
-if (sortedNumber.length < 2) {
+if (sortedNumbers.length < 2) {
   console.log(0);
 } else {
   console.log(sortedNumbers[1]);
