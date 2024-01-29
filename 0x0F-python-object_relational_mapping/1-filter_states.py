a1ad79ad9b230995a657792a21a7b67c962f5990
Kimshubb <issakimanifmk@gmail.com>
2024-01-29 13:29:05 -0800

    compare last commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,18 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def list_states_with_n(username, password, database):
-    """
-    Lists states from MySql db that starts with 'N'
-    Args: username(STR) : MySQL username
-          password(str) :MySQL password
-          database(str) :MySql database
-    returns:
-            none
-    """
-    connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username
-            password=password
-            db=database
-            )
-    cursor = connection.cursor()
-    query = SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;
-    cursor.execute(query)
-    results = cursor.fetchall()
-    print("Number of results: {}".format(len(results)))
-    for result in results:
-        print(result)
-    cursor.close()
-    connection.close()
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-        username = sys.argv[1]
-        password = sys.arg[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
 
-        filter_states_with_n(username, password, database)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
