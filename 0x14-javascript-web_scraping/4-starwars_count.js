6c2e48333f8f513416d5efe7b7ea901e3a976b22
Kimshubb <issakimanifmk@gmail.com>
2024-02-03 13:03:31 -0800

    width to with

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request(process.argv[2], function(error, response, body) {
     const results = JSON.parse(body).results;
     console.log(results.reduce(function(count, movie) {
       return movie.characters.find(function(character) {
-        return character.ends.Width('/18/');
+        return character.ends.With('/18/');
       }) ? count + 1 : count;
     }, 0));
   }
