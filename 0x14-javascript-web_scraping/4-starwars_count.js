2a6a5b1403e8dff17636b6a24849d93d99a6ffa4
Kimshubb <issakimanifmk@gmail.com>
2024-02-03 12:54:08 -0800

    error fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = 'request';
+const request = require('request');
 request(process.argv[2], function(error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
