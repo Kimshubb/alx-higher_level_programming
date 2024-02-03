66e4f65d97bdf4ed2572608c57389dfb3c7898d5
Kimshubb <issakimanifmk@gmail.com>
2024-02-03 12:50:54 -0800

    web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+fs.writeFile(process.argv[2], processargv[3], function(error) {
+  if (error) {
+    console.log(errer);
+    }
+});
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = 'request';
+request(process.argv[2], function(error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce(function(count, movie) {
+      return movie.characters.find(function(character) {
+        return character.ends.Width('/18/');
+      }) ? count + 1 : count;
+    }, 0));
+  }
+});
